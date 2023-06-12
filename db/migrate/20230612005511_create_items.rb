25809eb1636c1ee3fa33fda307022565e8be3365
qamarabdiaziz <tushez98@gmail.com>
2023-06-12 03:58:56 +0300

    Running migrations

---
+++ db/migrate/20230612005511_create_items.rb
@@ -0,0 +1,10 @@
+class CreateItems < ActiveRecord::Migration[6.1]
+  def change
+      create_table :items do |t|
+      t.string :name
+      t.boolean :completed
+      t.integer :category_id
+      t.timestamps
+    end  
+  end
+end
